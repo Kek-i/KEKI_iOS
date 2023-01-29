ff1501811ac949a5bdf889b8444a619f409a17d7
chowon <ryr0121@gmail.com>
2023-01-29 17:28:37 +0900

    initial commit

---
+++ KeKi/Global/Template/TemplateViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TemplateViewController.swift
+//  KeKi
+//
+//  Created by 김초원 on 2023/01/29.
+//
+
+import UIKit
+
+class TemplateViewController: UIViewController {
+
+    // MARK: - Variables, IBOutlet, ...
+    
+    // MARK: - Methods of LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Action Methods (IBAction, ...)
+
+    
+    // MARK: - Helper Methods (Setup Method, ...)
+
+}
+
+// MARK: - Extensions
---
+++ KeKi/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  KeKi
+//
+//  Created by 김초원 on 2023/01/29.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: - Variables, IBOutlet, ...
+    
+    // MARK: - Methods of LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Action Methods (IBAction, ...)
+
+    
+    // MARK: - Helper Methods (Setup Method, ...)
+
+}
+
+// MARK: - Extensions
