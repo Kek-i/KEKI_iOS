3fd34f505a8c299d44431f11bdd38175c0fded88
ryr0121 <ryr0121@gmail.com>
2023-01-31 00:26:56 +0900

    chore: 로그인 화면 중 가입자 선택 화면의 selected button의 UI 구현

--- KeKi/Scenes/Login/SelectUserCategoryViewController.swift
+++ KeKi/Scenes/Login/SelectUserCategoryViewController.swift
@@ -29,35 +29,52 @@ class SelectUserCategoryViewController: UIViewController {
     
     // MARK: - Action Methods (IBAction, ...)
     @IBAction func didTapBuyerButton(_ sender: UIButton) {
-        selectedResult = "buyer"
+        buyerButton.isSelected = !buyerButton.isSelected
+        setButtonBySelectStatus(buyerButton)
+        if (sellerButton.isSelected) && (buyerButton.isSelected) { didTapSellerButton(sellerButton) }
     }
     
     @IBAction func didTapSellerButton(_ sender: UIButton) {
-        selectedResult = "seller"
+        sellerButton.isSelected = !sellerButton.isSelected
+        setButtonBySelectStatus(sellerButton)
+        if (buyerButton.isSelected) && (sellerButton.isSelected) { didTapBuyerButton(buyerButton) }
     }
     
     @IBAction func didTapConfirmButton(_ sender: UIButton) {
-        if let selectedResult = selectedResult {
-            print(selectedResult)   // test print
-            if selectedResult == "buyer" {
-                // TODO: 구매자 프로필 화면으로 전환 구현
-                let storyboard = UIStoryboard(name: "UserProfileSetting", bundle: nil)
-                guard let buyerProfileSetViewController = storyboard.instantiateViewController(withIdentifier: "BuyerProfileSetViewController") as? BuyerProfileSetViewController else { return }
-                navigationController?.pushViewController(buyerProfileSetViewController, animated: true)
-                
-            } else if selectedResult == "seller" {
-                // TODO: 판매자 프로필 화면으로 전환 구현
-                let storyboard = UIStoryboard(name: "UserProfileSetting", bundle: nil)
-                guard let sellerProfileSetViewController = storyboard.instantiateViewController(withIdentifier: "SellerProfileSetViewController") as? SellerProfileSetViewController else { return }
-                navigationController?.pushViewController(sellerProfileSetViewController, animated: true)
-            }
+        if buyerButton.isSelected {
+            // TODO: 구매자 프로필 화면으로 전환 구현
+            let storyboard = UIStoryboard(name: "UserProfileSetting", bundle: nil)
+            guard let buyerProfileSetViewController = storyboard.instantiateViewController(withIdentifier: "BuyerProfileSetViewController") as? BuyerProfileSetViewController else { return }
+            navigationController?.pushViewController(buyerProfileSetViewController, animated: true)
+            
+        } else if sellerButton.isSelected {
+            // TODO: 판매자 프로필 화면으로 전환 구현
+            let storyboard = UIStoryboard(name: "UserProfileSetting", bundle: nil)
+            guard let sellerProfileSetViewController = storyboard.instantiateViewController(withIdentifier: "SellerProfileSetViewController") as? SellerProfileSetViewController else { return }
+            navigationController?.pushViewController(sellerProfileSetViewController, animated: true)
         }
     }
     
     // MARK: - Helper Methods (Setup Method, ...)
     private func setupLayout() {
-        [buyerButton, sellerButton].forEach { $0?.configuration?.imagePadding = 20 }
+        [buyerButton, sellerButton].forEach {
+            $0?.configuration?.imagePadding = 20
+            $0?.imageView?.layer.cornerRadius = ($0?.frame.width ?? 0)/2
+        }
         confirmButton.layer.cornerRadius = 25
+    }
+    
+    private func setButtonBySelectStatus(_ button: UIButton) {
+        if button.isSelected {
+            button.backgroundColor = UIColor.white
+            button.imageView?.layer.borderColor = UIColor(red: 209/255, green: 108/255, blue: 139/255, alpha: 1).cgColor
+            button.imageView?.layer.borderWidth = 3.0
+        } else {
+            button.backgroundColor = .white
+            button.imageView?.layer.borderColor = .none
+            button.imageView?.layer.borderWidth = 0
+        }
+        button.backgroundColor = .white
     }
 }
 
