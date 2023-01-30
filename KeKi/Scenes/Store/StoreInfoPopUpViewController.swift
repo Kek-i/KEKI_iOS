b4da84fce2999adf7ff85271837186d6b61df712
yoosang <yoo7969@naver.com>
2023-01-31 08:39:18 +0900

    feat: 사업자 정보 팝업 화면 터치 시 닫기 기능 구현

--- KeKi/Scenes/Store/StoreInfoPopUpViewController.swift
+++ KeKi/Scenes/Store/StoreInfoPopUpViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class StoreInfoPopUpViewController: UIViewController {
 
+    @IBOutlet var selfView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+
+                
+        selfView.addGestureRecognizer(tapGesture)
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        self.dismiss(animated: true)
     }
-    */
-
 }
