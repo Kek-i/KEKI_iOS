8381fb7bc148e82c4ae18e517b7cc73d872cfca9
yoosang <yoo7969@naver.com>
2023-01-31 10:47:19 +0900

    feat: 사업자 메인 화면 UI 구현 및 사업자 정보 팝업 화면 구현

---
+++ KeKi/Scenes/Store/StoreInfoPopUpViewController.swift
@@ -0,0 +1,26 @@
+//
+//  StoreInfoPopUpViewController.swift
+//  KeKi
+//
+//  Created by 유상 on 2023/01/31.
+//
+
+import UIKit
+
+class StoreInfoPopUpViewController: UIViewController {
+
+    @IBOutlet var selfView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+
+                
+        selfView.addGestureRecognizer(tapGesture)
+    }
+
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        self.dismiss(animated: true)
+    }
+}
