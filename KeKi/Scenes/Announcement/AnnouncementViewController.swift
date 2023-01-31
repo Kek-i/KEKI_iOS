edd59f622b9ec624fbd9c849544852b344f9194d
ryr0121 <ryr0121@gmail.com>
2023-01-31 10:43:03 +0900

    feat: 공지사항 목록 화면 UI 구현 (tabbar 및 공지 데이터는 임시)

--- KeKi/Scenes/Announcement/AnnouncementViewController.swift
+++ KeKi/Scenes/Announcement/AnnouncementViewController.swift
@@ -10,17 +10,65 @@ import UIKit
 class AnnouncementViewController: UIViewController {
 
     // MARK: - Variables, IBOutlet, ...
+    private var announcementList: [String] = [
+        "새로운 디저트가 등록되었어요!",
+        "크리스마스 기념 할인행사",
+        "새로운 디저트가 등록되었어요!",
+        "새로운 디저트가 등록되었어요! 어서 확인해보세요",
+        "새로운 디저트가 등록되었어요!",
+        "새로운 디저트가 등록되었어요!",
+        "새로운 디저트가 등록되었어요!",
+        "새로운 디저트가 등록되었어요!",
+        "새로운 디저트가 등록되었어요!",
+        "새로운 디저트가 등록되었어요!",
+        "새로운 디저트가 등록되었어요!",
+        "새로운 디저트가 등록되었어요!",
+        "새로운 디저트가 등록되었어요!",
+        "새로운 디저트가 등록되었어요!",
+    ]
+    
+    @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Methods of LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = false
+        setupTableView()
     }
     
     // MARK: - Action Methods (IBAction, ...)
 
     
     // MARK: - Helper Methods (Setup Method, ...)
-
+    private func setupTableView() {
+        tableView.separatorStyle = .none
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "AnnouncementCell", bundle: nil), forCellReuseIdentifier: "AnnouncementCell")
+    }
 }
 
 // MARK: - Extensions
+extension AnnouncementViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return announcementList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AnnouncementCell", for: indexPath)
+                as? AnnouncementCell else { return UITableViewCell() }
+        cell.titleLabel.text = announcementList[indexPath.row]
+        cell.setupLayout()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70   // default height
+    }
+}
+
+extension AnnouncementViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected cell --> \(indexPath.row)")
+    }
+}
