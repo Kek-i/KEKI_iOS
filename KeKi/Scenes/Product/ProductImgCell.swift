f497c8693e5ed5ccf6923118f4d719dffc776f5a
ryr0121 <ryr0121@gmail.com>
2023-02-01 09:52:07 +0900

    feat: 상품 상세 화면 UI 구현 (룩앤필을 위해 샘플 케이크 사진 추가 및 사용)

---
+++ KeKi/Scenes/Product/ProductImgCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductImgCell.swift
+//  KeKi
+//
+//  Created by 김초원 on 2023/02/01.
+//
+
+import UIKit
+
+class ProductImgCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
