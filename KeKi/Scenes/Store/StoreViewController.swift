8655cc1e041119a6f4f401f0f336df2f46b289c2
chayoosang <yoo7969@naver.com>
2023-02-20 19:01:45 +0900

    chore: Store 메인 화면 네비게이션 정리

--- KeKi/Scenes/Store/StoreViewController.swift
+++ KeKi/Scenes/Store/StoreViewController.swift
@@ -8,16 +8,11 @@
 import UIKit
 
 class StoreViewController: UIViewController {
-    
-    @IBOutlet weak var backButton: UIButton!
-    @IBOutlet weak var infoButton: UIButton!
-    @IBOutlet weak var messageButton: UIButton!
-    
+        
     @IBOutlet weak var storeImageView: UIImageView!
     
     @IBOutlet weak var storeNameLabel: UILabel!
     @IBOutlet weak var storeDescriptionLabel: UILabel!
-    
     
     @IBOutlet weak var orderButton: UIButton!
     
@@ -25,6 +20,7 @@ class StoreViewController: UIViewController {
         super.viewDidLoad()
         
         setupLayout()
+        setupNavigationBar()
     }
     
     func setupLayout() {
@@ -36,15 +32,38 @@ class StoreViewController: UIViewController {
         orderButton.layer.shadowRadius = 13
         orderButton.layer.shadowOpacity = 1.0
     }
-
     
-    @IBAction func showInfoPopUp(_ sender: Any) {
+    func setupNavigationBar() {
+        self.navigationController?.isNavigationBarHidden = false
+        
+        let backButton = UIBarButtonItem(image: UIImage(named: "chevron-right"), style: .done, target: self, action: #selector(backToScreen))
+        backButton.tintColor = .black
+        
+        let infoButton = UIBarButtonItem(image: UIImage(named: "info"), style: .done, target: self, action: #selector(showInfoPopUp))
+        infoButton.tintColor = .black
+        
+        let messageButton = UIBarButtonItem(image: UIImage(named: "icMessage"), style: .done, target: self, action: #selector(moveToMessage))
+        messageButton.tintColor = .black
+        
+        self.navigationItem.leftBarButtonItem = backButton
+        self.navigationItem.rightBarButtonItems = [infoButton, messageButton]
+    }
+
+    @objc func backToScreen() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func showInfoPopUp() {
         guard let infoPopUpVC = self.storyboard?.instantiateViewController(withIdentifier: "StoreInfoPopUpViewController") as? StoreInfoPopUpViewController else {return}
         
         infoPopUpVC.modalTransitionStyle = .coverVertical
         infoPopUpVC.modalPresentationStyle = .overCurrentContext
         
         self.present(infoPopUpVC, animated: true)
+    }
+    
+    @objc func moveToMessage() {
+        
     }
     
 }
