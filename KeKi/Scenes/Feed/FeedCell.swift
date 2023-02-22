26bd0b9cc1da743a83c6e0ba6b3a3b32a94e95e5
ryr0121 <ryr0121@gmail.com>
2023-02-22 17:21:24 +0900

    chore: 불필요한 주석 및 extension 삭제

--- KeKi/Scenes/Feed/FeedCell.swift
+++ KeKi/Scenes/Feed/FeedCell.swift
@@ -89,7 +89,6 @@ class FeedCell: UITableViewCell {
         pageControl.currentPage = 0
         
         dessertNameButton.setTitle(data.dessertName, for: .normal)
-//        dessertNameButton.titleLabel.font = UIFont.boldSystemFont(ofSize: 17)
         descriptionTextView.text = data.description
         descriptionTextView.sizeToFit()
         
@@ -159,34 +158,5 @@ extension FeedCell: UICollectionViewDelegateFlowLayout {
         if pageControl.currentPage != newPage {
             pageControl.currentPage = newPage
         }
-    }
-}
-
-
-extension String {
-    func attributed(of searchString: String, value: [NSAttributedString.Key: Any]) -> NSMutableAttributedString {
-    
-        let attributedString = NSMutableAttributedString(string: self)
-        let length = self.count
-        
-        var range = NSRange(location: 0, length: length)
-        var rangeArray = [NSRange]()
-
-        while range.location != NSNotFound {
-            range = (attributedString.string as NSString).range(of: searchString, options: .caseInsensitive, range: range)
-            rangeArray.append(range)
-
-            if range.location != NSNotFound {
-                range = NSRange(location: range.location + range.length, length: self.count - (range.location + range.length))
-            }
-        }
-        
-        rangeArray.forEach { range in
-            value.forEach { values in
-                attributedString.addAttribute(values.key, value: values.value, range: range)
-            }
-        }
-
-        return attributedString
     }
 }
