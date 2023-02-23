4f3075195f4106bb83991055f69a6ae31ad38eb6
ryr0121 <ryr0121@gmail.com>
2023-02-23 17:43:45 +0900

    feat: 스토어 메인에서 피드 목록 중 하나 탭 -> 상품 상세로 연결 구현 (포커싱 수정 필요)

--- KeKi/Scenes/Home/HomeTableViewCell.swift
+++ KeKi/Scenes/Home/HomeTableViewCell.swift
@@ -107,13 +107,3 @@ extension HomeTableViewCell: UICollectionViewDelegateFlowLayout {
     }
 }
 
-extension UIResponder {
-    func next<T:UIResponder>(ofType: T.Type) -> T? {
-        let r = self.next
-        if let r = r as? T ?? r?.next(ofType: T.self) {
-            return r
-        } else {
-            return nil
-        }
-    }
-}
--- KeKi/Scenes/Store/StoreImageViewController.swift
+++ KeKi/Scenes/Store/StoreImageViewController.swift
@@ -63,4 +63,16 @@ extension StoreImageViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 5, left: 5, bottom: 0, right: 5)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard.init(name: "Feed", bundle: nil)
+        guard let feedViewController = storyboard.instantiateViewController(withIdentifier: "FeedViewController") as? FeedViewController else { return }
+        
+        feedViewController.feedData = self.feeds
+        if let vc = self.next(ofType: UIViewController.self) {
+            vc.tabBarController?.tabBar.isHidden = true
+            vc.navigationController?.pushViewController(feedViewController, animated: true)
+        }
+        // TODO: 선택된 피드가 상세 화면에서 포커싱되도록 수정 (현재 어떤 피드를 탭해도 0번째 피드부터 보임)
+    }
 }
