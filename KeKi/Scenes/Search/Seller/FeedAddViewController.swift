557ae8465c6e39b9c42918c18ee6488d9620ec7f
차유상 <yoo7969@naver.com>
2023-02-23 09:44:19 +0900

    feat: 피드 추가 UI Storyboard

---
+++ KeKi/Scenes/Search/Seller/FeedAddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FeedAddViewController.swift
+//  KeKi
+//
+//  Created by 유상 on 2023/02/23.
+//
+
+import UIKit
+
+class FeedAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
